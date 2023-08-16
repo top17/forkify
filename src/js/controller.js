e5c2fa44d0694c87a5025b39da5ef6088ae273c6
top17 <vladimirtopalovic1417@gmail.com>
2023-08-17 01:28:10 +0200

    bugfix

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,8 +11,6 @@ import homeView from './views/homeView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-// https://forkify-api.herokuapp.com/v2
-
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
